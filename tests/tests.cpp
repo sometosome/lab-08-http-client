338bf2502c499acc48c2d31795f1ec78ac3a747e
sadovnikovsergey <sergcheh27@gmail.com>
2022-04-03 15:17:01 -0700

    ver 0.1.3

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,11 +1,8 @@
 // Copyright 2021 Your Name <your_email>
 
+#include <gtest/gtest.h>
 #include <stdexcept>
 
-#include <gtest/gtest.h>
-
-#include <processing_answer.hpp>
-
 TEST(Example, EmptyTest) {
-    EXPECT_THROW(example(), std::runtime_error);
+  EXPECT_TRUE(true);
 }
